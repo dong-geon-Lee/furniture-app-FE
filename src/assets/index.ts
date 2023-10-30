627933f81dba71089a1e1b480c9b77333341e4e7
developer <ch9716@naver.com>
2023-10-30 19:16:17 +0900

    assets 폴더 index 설정

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,1 +1,5 @@
-export { default as landing } from "./3969008 1.jpg";
+export { default as boarding } from "./boarding.jpg";
+export { default as group } from "./Group.svg";
+export { default as container } from "./container.svg";
+export { default as Visibility } from "@mui/icons-material/Visibility";
+export { default as VisibilityOff } from "@mui/icons-material/VisibilityOff";
