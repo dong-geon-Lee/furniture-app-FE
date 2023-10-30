a66e25eaf8f9a7a9f38d812b8288752635154093
developer <ch9716@naver.com>
2023-10-30 19:14:36 +0900

    리액트 라우터 경로 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Boarding from "./pages/Boarding/Boarding";
+import Login from "./pages/Login/Login";
 
 const App = () => {
   return (
     <div className="container">
-      <Boarding />
+      <Routes>
+        <Route path="/" element={<Boarding />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };
