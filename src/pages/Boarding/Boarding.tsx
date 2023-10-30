6fe49c1ca8ca6845505baec83241d3fbf4fe6500
developer <ch9716@naver.com>
2023-10-30 19:15:42 +0900

    useNavigate 사용하여 login 경로 이동

--- src/pages/Boarding/Boarding.tsx
+++ src/pages/Boarding/Boarding.tsx
@@ -1,10 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 import * as S from "./styles";
 import * as A from "../../assets";
 
 const Boarding = () => {
+  const navigate = useNavigate();
+
   return (
     <S.Container>
-      <S.Img src={A.landing} alt="landing" />
+      <S.Img src={A.boarding} alt="boarding" />
       <S.Div>
         <S.H1>Make your</S.H1>
         <S.H1 className="h1">Home Beautiful</S.H1>
@@ -12,7 +16,7 @@ const Boarding = () => {
           The best simple place where you discover most wonderful furnitures and
           make your home beautiful
         </S.P>
-        <S.Button>Get started</S.Button>
+        <S.Button onClick={() => navigate("/login")}>Get started</S.Button>
       </S.Div>
     </S.Container>
   );
