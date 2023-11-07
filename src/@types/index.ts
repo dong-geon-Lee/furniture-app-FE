72bd044ebe9df428f29cdc07fecebf9f5fd1896e
developer <ch9716@naver.com>
2023-11-07 20:34:54 +0900

    제품 타입 인터페이스 추가

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -9,3 +9,11 @@ export interface ILoginProps {
   email: string;
   password: string;
 }
+
+export interface IProductProps {
+  name: string;
+  description: string;
+  price: string;
+  imageURL: string;
+  category: string;
+}
