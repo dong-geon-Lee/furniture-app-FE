4c5f6a8287e104ab8222b1a98e9a2a1fc22b593a
developer <ch9716@naver.com>
2023-11-08 12:55:42 +0900

    ProductButton 컴포넌트 구현 및 스타일링

--- src/components/ProductButton/ProductButton.tsx
+++ src/components/ProductButton/ProductButton.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { IPrductButtonProps } from "../../@types";
+import * as S from "./styles";
 
-const ProductButton = () => {
-  return <div>ProductButton</div>;
+const ProductButton = ({
+  handleCategorySelect,
+  selectedCategory,
+  image,
+  categoryName,
+}: IPrductButtonProps) => {
+  return (
+    <S.Box
+      onClick={() => handleCategorySelect(categoryName)}
+      className={selectedCategory === categoryName ? "active" : ""}
+    >
+      <S.Logo src={image} alt={image} />
+      <S.Text>{categoryName}</S.Text>
+    </S.Box>
+  );
 };
 
 export default ProductButton;
--- src/components/ProductButton/styles.ts
+++ src/components/ProductButton/styles.ts
@@ -1,3 +1,46 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const Box = styled.div`
+  border-radius: 1.2rem;
+  width: 4.4rem;
+  height: 4.4rem;
+  background: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+
+  &.active {
+    background: #303030;
+  }
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(1.03);
+
+    & p {
+      color: #242424;
+    }
+  }
+`;
+
+export const Logo = styled.img`
+  width: 2rem;
+  height: 2rem;
+  object-fit: cover;
+  display: block;
+`;
+
+export const Text = styled.p`
+  color: #999;
+  font-family: "Nunito Sans";
+  font-size: 1.4rem;
+  font-weight: 600;
+  line-height: normal;
+  position: absolute;
+  bottom: -40%;
+  transform: translateY(40%);
+`;
