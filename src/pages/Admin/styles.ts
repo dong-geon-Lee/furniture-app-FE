afca2bab57569aef6e0f626890807480f9e0a7d8
developer <ch9716@naver.com>
2023-11-09 09:51:13 +0900

    Admin 페이지 코드 리팩토링

--- src/pages/Admin/styles.ts
+++ src/pages/Admin/styles.ts
@@ -1,57 +1,56 @@
-import { Alert as MuiAlert, TextField, Typography } from "@mui/material";
-import styled from "styled-components";
-import { Button as MuiButton, Box as MuiBox } from "@mui/material";
+// import { Alert as MuiAlert, TextField, Typography } from "@mui/material";
+// import styled from "styled-components";
+// import { Button as MuiButton, Box as MuiBox } from "@mui/material";
 
-export const Container = styled.div`
-  /* width: 100vw;
-  height: 100vh; */
-  width: 100%;
-  height: 100%;
-  position: relative;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  margin-top: 10rem;
+// export const Container = styled.div`
 
-  & .css-wnkddu-MuiPaper-root-MuiAlert-root {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 100%;
-  }
-`;
+//   width: 100%;
+//   height: 100%;
+//   position: relative;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   flex-direction: column;
+//   margin-top: 10rem;
 
-export const H1 = styled(Typography)``;
+//   & .css-wnkddu-MuiPaper-root-MuiAlert-root {
+//     position: absolute;
+//     top: 50%;
+//     left: 50%;
+//     transform: translate(-50%, -50%);
+//     width: 100%;
+//   }
+// `;
 
-export const Alert = styled(MuiAlert)``;
+// export const H1 = styled(Typography)``;
 
-export const Form = styled(MuiBox)`
-  width: 100%;
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  align-content: center;
-  justify-items: left;
-  gap: 1.6rem;
-`;
+// export const Alert = styled(MuiAlert)``;
 
-export const Input = styled(TextField)`
-  width: 100%;
-  padding: 2rem;
-`;
+// export const Form = styled(MuiBox)`
+//   width: 100%;
+//   display: grid;
+//   grid-template-columns: repeat(1, 1fr);
+//   align-content: center;
+//   justify-items: left;
+//   gap: 1.6rem;
+// `;
 
-export const Div = styled.div`
-  display: grid;
-  grid-template-columns: repeat(1, 1fr);
-  width: 100%;
-`;
+// export const Input = styled(TextField)`
+//   width: 100%;
+//   padding: 2rem;
+// `;
 
-export const Button = styled(MuiButton)`
-  width: 100%;
+// export const Div = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(1, 1fr);
+//   width: 100%;
+// `;
 
-  &.css-sghohy-MuiButtonBase-root-MuiButton-root {
-    font-family: inherit;
-    font-size: 1.2rem;
-  }
-`;
+// export const Button = styled(MuiButton)`
+//   width: 100%;
+
+//   &.css-sghohy-MuiButtonBase-root-MuiButton-root {
+//     font-family: inherit;
+//     font-size: 1.2rem;
+//   }
+// `;
