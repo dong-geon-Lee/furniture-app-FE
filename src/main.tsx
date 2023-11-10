d9b65c51b13314d42da1f7107d1d6e2e478e1f24
developer <ch9716@naver.com>
2023-11-10 16:02:33 +0900

    리덕스 툴킷 초기상태 정의

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
+import { Provider } from "react-redux";
+import { store } from "./store/index";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <>
+  <Provider store={store}>
     <BrowserRouter>
       <SnackbarProvider>
         <App />
       </SnackbarProvider>
     </BrowserRouter>
-  </>
+  </Provider>
 );
