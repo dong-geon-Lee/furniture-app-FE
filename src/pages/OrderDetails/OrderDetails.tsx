ebdb34b71eece9779dbd2d955f28aa0d9260d418
developer <ch9716@naver.com>
2023-11-21 11:13:45 +0900

    OrderDetails 목업 구현

---
+++ src/pages/OrderDetails/OrderDetails.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+import * as S from "./styles";
+import TopHeader from "../../components/TopHeader/TopHeader";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
+
+const OrderDetails = () => {
+  const { token } = useSelector((state: RootState) => state.users);
+  const location = useLocation();
+  const order = location.state;
+  const formattedPrice = new Intl.NumberFormat("ko-KR");
+
+  console.log(order, location.pathname);
+
+  // useEffect(() => {
+  //   const fetchDatas = async () => {
+  //     const response = await axios.get("http://localhost:5000/orders");
+  //     const { data } = await axios.get("http://localhost:5000/users/me", {
+  //       headers: { Authorization: `Bearer ${token}` },
+  //     });
+
+  //     const selectedData = response.data.filter(
+  //       (item: any) => item.user.id === data.id
+  //     );
+  //     console.log(selectedData);
+  //   };
+  //   fetchDatas();
+  // }, []);
+
+  return (
+    <S.Container>
+      <TopHeader path="orders" title="주문상세" />
+      <S.Section>
+        <S.Div className="orders__box" key={order.id}>
+          <S.Div className="orders__header">
+            <S.H1>{order.impUid}</S.H1>
+            {/* <S.Img src={A.xIcon} alt={A.xIcon} /> */}
+          </S.Div>
+
+          <S.Div className="orders__contents">
+            <S.Label>
+              주문수량: <S.Span>{order.quantity}개</S.Span>
+            </S.Label>
+            <S.Label>
+              <S.Span>{formattedPrice.format(order.paidAmount)}원</S.Span>
+            </S.Label>
+          </S.Div>
+          <S.Div className="orders__results">
+            <S.H3>
+              {order.status === "paid" ? "구매확정완료" : "승인대기중"}
+            </S.H3>
+          </S.Div>
+        </S.Div>
+      </S.Section>
+    </S.Container>
+  );
+};
+
+export default OrderDetails;
