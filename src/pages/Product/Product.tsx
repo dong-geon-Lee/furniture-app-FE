0ba183fb6cd576e22d11c81b19da408393b3719c
developer <ch9716@naver.com>
2023-11-23 17:30:25 +0900

    Product 컴포넌트, 해당 제품의 수량 조절과 카트 담기 기능 추가

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -1,22 +1,93 @@
+import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
 import Header from "../../components/Header/Header";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
+import { RootState } from "../../store";
+import { getItems } from "../../store/features/carts/cartsSlice";
+
+import axios from "axios";
+
 import * as S from "./styles";
 import * as A from "../../assets";
+import { getUser } from "../../store/features/users/usersSlice";
 
 const Product = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const location = useLocation();
-  const items = location.state;
+  const currentProduct = location.state;
+
+  const { cartItems } = useSelector((state: RootState) => state.carts);
+  const { user, token } = useSelector((state: RootState) => state.users);
+
+  console.log("cartItems before .find call:", cartItems);
+  const findCartId = cartItems?.find(
+    (cartItem) => cartItem?.product?.id === currentProduct?.id
+  );
+
+  const handleIncToCart = async (qty: number) => {
+    if (findCartId !== undefined) {
+      const response = await axios.patch(
+        `http://localhost:5000/carts/${findCartId?.id}`,
+        {
+          quantity: qty,
+        }
+      );
+
+      const datas = response.data;
+      const newCarts = cartItems.map((cartItem) =>
+        cartItem.id === datas.id ? (cartItem = datas) : cartItem
+      );
+
+      dispatch(getItems(newCarts));
+    } else {
+      alert("먼저 장바구니에 담아주세요");
+      return;
+    }
+  };
+
+  console.log(user);
+  console.log(currentProduct);
+  console.log(cartItems);
+
+  const handleAddCart = async () => {
+    const response = await axios.post("http://localhost:5000/carts", {
+      userId: user?.id,
+      productId: currentProduct.id,
+      quantity: 1,
+    });
+
+    dispatch(getItems(response.data));
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:5000/carts");
+      dispatch(getItems(response.data));
+    };
+    fetchData();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:5000/users/me", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      dispatch(getUser(response.data));
+    };
+    fetchData();
+  }, [token, dispatch]);
 
   return (
     <S.Container>
       <Header activeClass="header" />
 
       <S.Div className="product__image">
-        <S.ProductImage $image={items?.imageURL} />
+        <S.ProductImage $image={currentProduct?.imageURL} />
         <S.Img
           src={A.back}
           alt="product1"
@@ -25,15 +96,21 @@ const Product = () => {
         />
       </S.Div>
       <S.Div className="product__info">
-        <S.H1 className="product__title">{items?.name}</S.H1>
+        <S.H1 className="product__title">{currentProduct?.name}</S.H1>
         <S.Div className="product__box">
-          <S.H1 className="product__price">$ {items?.price}</S.H1>
+          <S.H1 className="product__price">$ {currentProduct?.price}</S.H1>
           <S.Div className="product__qty">
-            <S.Button className="product__count">
+            <S.Button
+              className="product__count"
+              onClick={() => handleIncToCart(findCartId?.quantity + 1)}
+            >
               <S.Img src={A.btn1} alt="btn1" />
             </S.Button>
-            <S.H2 className="product__text">01</S.H2>
-            <S.Button className="product__count">
+            <S.H2 className="product__text">{findCartId?.quantity || 1}</S.H2>
+            <S.Button
+              className="product__count"
+              onClick={() => handleIncToCart(findCartId?.quantity - 1)}
+            >
               <S.Img src={A.btn2} alt="btn2" />
             </S.Button>
           </S.Div>
@@ -45,7 +122,7 @@ const Product = () => {
         </S.Div>
         <S.Div>
           <S.P className="description">
-            {items?.name} {items?.description}
+            {currentProduct?.name} {currentProduct?.description}
           </S.P>
           <S.Div className="product__click">
             <S.Button className="product__markers">
@@ -58,7 +135,15 @@ const Product = () => {
                 }}
               />
             </S.Button>
-            <S.Button className="product__cart">Add to cart</S.Button>
+            <S.Button
+              className="product__cart"
+              disabled={cartItems.some(
+                (cartItem) => cartItem.product?.id === currentProduct?.id
+              )}
+              onClick={() => handleAddCart()}
+            >
+              Add to cart
+            </S.Button>
           </S.Div>
         </S.Div>
       </S.Div>
